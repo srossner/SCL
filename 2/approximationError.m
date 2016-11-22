fa3b18b15120de6e459bc1fcd8191cb7a443d57c
Sebastian Rossner <Sebastian Rossner>
2016-11-22 13:11:33 +0100

    approximationError.m

--- 2/approximationError.m
+++ 2/approximationError.m
@@ -1,9 +1,9 @@
 function [ E ] = approximationError( pt , pt_, dt, tend )
 %UNTITLED Approximation Error using the euclidean norm and the timstep size
-%   this function calculate rhe Approximation Error for two vector values
-%   of functions starting on 0 goimngt to tend and using the euclidean norm
-%   to claculate the error and multiplys it with the proportion of the
-%   timestep to the end time 
+%   this function calculate rhe Approximation error for two vector using
+%   the euclidean norm to claculate the error and multiplys it with the 
+%   proportion of timestep to the time interval in our case equal to tend
+%   as we are starting on t0 = 0
     E = sqrt( dt/tend ) * norm(pt_-pt);
 
 end
