5e659862361756c2fb0f12433af80e883e1501b4
Your Name <sebastian.rossner@tum.de>
2016-11-23 11:34:27 +0100

    added submishen

---
+++ 2/group14_assignment2/myPlot.m
@@ -0,0 +1,24 @@
+function myPlot()
+close all; %to close all plots 
+clear all;
+clc;
+
+% Time interval from -12 to 15 with 0.01 as step size for the plot 
+t = -12:0.01:15;
+
+my_Plot = figure; 
+hold on;
+%set window name 
+set(my_Plot,'name','Analytical Solution','numbertitle','off');
+% p() is the analytical solution
+plot(t, p(t));
+
+%set axis for best lock use window resolution 1920x1080
+axis([-12 15 -2 12]) ;
+
+% creat a label for the plot 
+legend('p(t)')
+
+%set title of the plot
+title('Graph of analytical solution for the ordinary differential equation');
+end
---
+++ 2/group14_assignment2/p_.m
@@ -0,0 +1,6 @@
+function [ pt_ ] = p_( p )
+%UNTITLED this function is the ordinary differential equation for the second
+%assignment
+    pt_ = (1 - (p./10)).*p ;
+end
+
