4f34acb402dc918d1424bbf31086823c4471ecb1
Your Name <sebastian.rossner@tum.de>
2016-12-09 15:39:49 +0100

    added axis for direction field

--- 3/matlab/dirField.m
+++ 3/matlab/dirField.m
@@ -1,7 +1,7 @@
-function dirField(p_)
-    [p,q]=meshgrid(-20:1:20,-20:1:20);
+function dirField(p_, y_axis,x_axis)
+    [p,q]=meshgrid( y_axis,x_axis);
     dP=p_(p);
     dQ=ones(size(q));
     L=sqrt((dP.^2)+(dQ.^2));
-    quiver(q,p,dQ./L,dP./L,.5,'r'), axis tight;
+    quiver(q,p,dQ./L,dP./L,.5,'b'), axis tight;
 end
