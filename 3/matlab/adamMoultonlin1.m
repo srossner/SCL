7046e833d6cdbc50e2c1a2fe56b5710bd37a7f52
Sebastian Rossner <Sebastian Rossner>
2016-12-11 14:39:22 +0100

    maxIterations

--- 3/matlab/adamMoultonlin1.m
+++ 3/matlab/adamMoultonlin1.m
@@ -3,7 +3,8 @@ function [ pt ] = adamMoultonlin1( p0, tstep, tend)
 %with respect to y, initial value p0, the stepsize dt and the end time tend. The output
 %of the function is a vector containing all computed approximate values for
 %y using Adam Mouldton Method by linerisation method 1
+    maxIterations = 200;
     F = @(yn1,yn,dt) yn1-yn-(3.5*dt*((yn*(1-(yn/10)))+(yn*(1-(yn1/10)))));
     F_= @(yn1,yn,dt) 1+(.35*dt*yn);
-    [ pt ] = implicitMethod( F, F_, p0, tstep, tend);
+    [ pt ] = implicitMethod( F, F_, p0, tstep, tend, maxIterations);
 end
--- 3/matlab/adamMoultonlin2.m
+++ 3/matlab/adamMoultonlin2.m
@@ -3,7 +3,8 @@ function [ pt ] = adamMoultonlin2(p0, tstep, tend)
 %with respect to y, initial value p0, the stepsize dt and the end time tend. The output
 %of the function is a vector containing all computed approximate values for
 %y using Adam Mouldton Method by linerisation method 2
+    maxIterations = 200;
     F = @(yn1,yn,dt) yn1-yn-(3.5*dt*((yn*(1-(yn/10)))+(yn1*(1-(yn/10)))));
     F_= @(yn1,yn,dt) 1-(3.5*dt*(1-0.1*yn));
-    [ pt ] = implicitMethod( F, F_, p0, tstep, tend);
+    [ pt ] = implicitMethod( F, F_, p0, tstep, tend, maxIterations);
 end
