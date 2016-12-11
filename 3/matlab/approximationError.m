d0d4d53f7346aeeb1b7add7252893817e1420a0c
Sebastian Rossner <Sebastian Rossner>
2016-12-11 16:27:22 +0100

    setup for Adams Moulton method dt = 0.5

--- 3/matlab/approximationError.m
+++ 3/matlab/approximationError.m
@@ -5,15 +5,20 @@ function [ E ] = approximationError( pt , pt_, dt, tend )
 %   proportion of timestep to the time interval, in our case equal to tend
 %   as we are starting on t0 = 0
 
-    if(size(pt_) == size(pt))
-        E = sqrt( dt/tend ) * norm(pt_-pt); %norm(X)is same as norm(X,2)
+    if(isnan(pt_))
+        E = NaN;
     else
-        step = (size(pt)-1)/(size(pt_)-1 );
-        pt_e = 1:size(pt_);
-        for i = pt_e
-            pt_e(i) = pt_(i) - pt(i*step);
+
+        if(size(pt_) == size(pt))
+            E = sqrt( dt/tend ) * norm(pt_-pt); %norm(X)is same as norm(X,2)
+        else
+            step = (size(pt)-1)/(size(pt_)-1 );
+            pt_e = 1:size(pt_);
+            for i = pt_e
+                pt_e(i) = pt_(i) - pt(i*step);
+            end
+            E = sqrt( dt/tend ) * norm(pt_e); %norm(X)is same as norm(X,2)
         end
-        E = sqrt( dt/tend ) * norm(pt_e); %norm(X)is same as norm(X,2)
     end
     
 end
