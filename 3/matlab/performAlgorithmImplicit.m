610276b1dd36e70e6bf43b7edafa434b3f339704
Sebastian Rossner <Sebastian Rossner>
2016-12-11 16:03:16 +0100

    as in explicit fix

--- 3/matlab/performAlgorithmImplicit.m
+++ 3/matlab/performAlgorithmImplicit.m
@@ -15,7 +15,7 @@ function [ solution ] = performAlgorithmImplicit( t0, tend, dt_start, iterations
         'numbertitle','off');
     
     %set title of plot
-    title( strcat(string('Graph of '), String_AlgorithmName , string(' solution:')));
+    title( sprintf( 'Graph of %s solution:', String_AlgorithmName ));
     axis([-0.5 5.5 -0.5 20.5]) ;
     a_x = gca;
     a_x.XAxisLocation = 'origin';
