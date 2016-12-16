f75c29946cdb5d415abf7d786de8da6c4fd2d273
Sebastian Roßner <github@rossner.in>
2016-12-16 20:57:31 +0100

    test

--- 4/matlab/GaussSeidel.m
+++ 4/matlab/GaussSeidel.m
@@ -4,14 +4,16 @@ function [ x ] = GaussSeidel( A , x0, b , maxIterations )
     if m ~= n
         error('Matrix A must be square');
     end
+    
     err = 1;
     iteration = 0;
+    
     while err >= 0.01 || maxIterations <= iteration
         
        L = tril(A);
        U = triu(A) - diag(diag(A));
        
-       t = ( L - diag(diag(A)) )^-1
+       t = ( L - diag(diag(A)) )^-1;
        
        x = t * U * x0 + t *b;
 
