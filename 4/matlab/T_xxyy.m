73d195fef23d2a58ded4f7d98d817e9c15632a7e
Sebastian Roßner <github@rossner.in>
2016-12-16 18:47:32 +0100

    initial

---
+++ 4/matlab/T_xxyy.m
@@ -0,0 +1,6 @@
+function [ t ] = T_xxyy( x, y )
+
+	t = -2*pi^2 * sin(pi.* x') * sin ( pi.* y);
+
+end
+
--- 4/matlab/worksheet_4.m
+++ 4/matlab/worksheet_4.m
@@ -3,6 +3,25 @@ clear all;
 clc;
 
 % plot analitical Solution 
-x = 0:0.01:1;
-y = 0:0.01:1;
-surfc(x, y, T_xy(x,y));
+dt_x = 0.1;
+dt_y = 0.1;
+x = dt_x:dt_x:(1-dt_x);
+y = dt_y:dt_y:(1-dt_y);
+sol =  T_xy(x,y);
+surfc( y,x,sol);
+
+size_x = length(x);
+size_y = length(y);
+M = createM(size_x, size_y);
+figure
+spy(M);
+
+x1 = T_xxyy(x, y);
+x3 = reshape(x1,[(size_x * size_y) , 1 ]);
+
+sol2 = M\x3;
+
+figure
+my_Sol = reshape(sol2,[size_x, size_y]);
+
+surfc( y,x,my_Sol);
