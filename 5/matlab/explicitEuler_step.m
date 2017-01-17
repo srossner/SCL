ab4df957d682ad3d23b8dd24ed7b3c8b4d3674db
Sebastian Roßner <github@rossner.in>
2017-01-17 20:16:34 +0100

    added Implicid

--- 5/matlab/explicitEuler_step.m
+++ 5/matlab/explicitEuler_step.m
@@ -1,15 +1,18 @@
-function [ solution ] = explicitEuler_step( N_x, N_y, dt, solution_previous)
+function [ solution_next_timestep ] = explicitEuler_step( N_x, N_y, dt, solution_current_timestep )
 
     h_x_p2 = (1/N_x)^2;
     h_y_p2 = (1/N_y)^2;
 
-    solution = zeros(N_x, N_y);
+    solution_next_timestep = zeros(N_x, N_y);
+    
     for i = 2:N_x-1
         for j = 2:N_y-1
-            k_x = ( ( solution_previous(i-1, j) - (2 * solution_previous(i, j)) + solution_previous(i + 1, j) ) / h_x_p2 );
-            k_y = ( ( solution_previous(i, j-1) - (2 * solution_previous(i, j)) + solution_previous(i, j + 1) ) / h_y_p2 );
+            k_x = ( ( solution_current_timestep(i-1, j) - (2 * solution_current_timestep(i, j)) + solution_current_timestep(i + 1, j) ) / h_x_p2 );
+            k_y = ( ( solution_current_timestep(i, j-1) - (2 * solution_current_timestep(i, j)) + solution_current_timestep(i, j + 1) ) / h_y_p2 );
+            
             k = k_x + k_y;
-            solution(i, j) = solution_previous(i, j) + dt * k;
+            
+            solution_next_timestep(i, j) = solution_current_timestep(i, j) + dt * k;
         end
     end
 
